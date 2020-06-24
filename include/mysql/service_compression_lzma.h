a399f27389e68c5250795ec4891ae373de98a9eb
Kartik Soneji <kartiksoneji@rocketmail.com>
2020-06-24 17:07:58 +0530

    Resolve naming conflicts.

--- include/mysql/service_compression_lzma.h
+++ include/mysql/service_compression_lzma.h
@@ -12,40 +12,40 @@ extern "C" {
 #include <stddef.h>
 
 typedef enum {
-	LZMA_OK                 = 0,
-	LZMA_STREAM_END         = 1,
-	LZMA_NO_CHECK           = 2,
-	LZMA_UNSUPPORTED_CHECK  = 3,
-	LZMA_GET_CHECK          = 4,
-	LZMA_MEM_ERROR          = 5,
-	LZMA_MEMLIMIT_ERROR     = 6,
-	LZMA_FORMAT_ERROR       = 7,
-	LZMA_OPTIONS_ERROR      = 8,
-	LZMA_DATA_ERROR         = 9,
-	LZMA_BUF_ERROR          = 10,
-	LZMA_PROG_ERROR         = 11,
-} lzma_ret;
+	SERIVCE_LZMA_OK                 = 0,
+	SERIVCE_LZMA_STREAM_END         = 1,
+	SERIVCE_LZMA_NO_CHECK           = 2,
+	SERIVCE_LZMA_UNSUPPORTED_CHECK  = 3,
+	SERIVCE_LZMA_GET_CHECK          = 4,
+	SERIVCE_LZMA_MEM_ERROR          = 5,
+	SERIVCE_LZMA_MEMLIMIT_ERROR     = 6,
+	SERIVCE_LZMA_FORMAT_ERROR       = 7,
+	SERIVCE_LZMA_OPTIONS_ERROR      = 8,
+	SERIVCE_LZMA_DATA_ERROR         = 9,
+	SERIVCE_LZMA_BUF_ERROR          = 10,
+	SERIVCE_LZMA_PROG_ERROR         = 11,
+} SERIVCE_lzma_ret;
 
 typedef struct {
 	void *(*alloc)(void *opaque, size_t nmemb, size_t size);
 	void (*free)(void *opaque, void *ptr);
 	void *opaque;
-} lzma_allocator;
+} SERIVCE_lzma_allocator;
 
 typedef enum {
-	LZMA_CHECK_NONE     = 0,
-	LZMA_CHECK_CRC32    = 1,
-	LZMA_CHECK_CRC64    = 4,
-	LZMA_CHECK_SHA256   = 10
-} lzma_check;
+	SERIVCE_LZMA_CHECK_NONE     = 0,
+	SERIVCE_LZMA_CHECK_CRC32    = 1,
+	SERIVCE_LZMA_CHECK_CRC64    = 4,
+	SERIVCE_LZMA_CHECK_SHA256   = 10
+} SERIVCE_lzma_check;
 
 extern struct compression_service_lzma_st {
-  lzma_ret (*lzma_stream_buffer_decode_ptr)(uint64_t *, uint32_t, 
-       const lzma_allocator *, const uint8_t *, size_t *,
+  SERIVCE_lzma_ret (*SERIVCE_lzma_stream_buffer_decode_ptr)(uint64_t *, uint32_t, 
+       const SERIVCE_lzma_allocator *, const uint8_t *, size_t *,
 	   size_t, uint8_t *, size_t *, size_t);
   
-  lzma_ret (*lzma_easy_buffer_encode_ptr)(uint32_t, lzma_check,
-		const lzma_allocator *,
+  SERIVCE_lzma_ret (*SERIVCE_lzma_easy_buffer_encode_ptr)(uint32_t, SERIVCE_lzma_check,
+		const SERIVCE_lzma_allocator *,
 		const uint8_t *, size_t,
 		uint8_t *, size_t *, size_t);
 } *compression_service_lzma;
@@ -53,8 +53,8 @@ extern struct compression_service_lzma_st {
 
 #ifdef MYSQL_DYNAMIC_PLUGIN
 
-#define lzma_stream_buffer_decode(a, b, c, d, e, f, g, h, i) compression_service_lzma->lzma_stream_buffer_decode_ptr(a, b, c, d, e, f, g, h, i)
-#define lzma_easy_buffer_encode(a, b, c, d, e, f, g, h) compression_service_lzma->lzma_easy_buffer_encode_ptr(a, b)
+#define SERIVCE_lzma_stream_buffer_decode(a, b, c, d, e, f, g, h, i) compression_service_lzma->SERIVCE_lzma_stream_buffer_decode_ptr(a, b, c, d, e, f, g, h, i)
+#define SERIVCE_lzma_easy_buffer_encode(a, b, c, d, e, f, g, h) compression_service_lzma->SERIVCE_lzma_easy_buffer_encode_ptr(a, b)
 
 #else
 
@@ -72,17 +72,17 @@ extern struct compression_service_lzma_st {
 #	endif
 #endif
 
-extern lzma_ret lzma_stream_buffer_decode(
+extern SERIVCE_lzma_ret SERIVCE_lzma_stream_buffer_decode(
 		uint64_t *, uint32_t,
-		const lzma_allocator *,
+		const SERIVCE_lzma_allocator *,
 		const uint8_t *, size_t *, size_t,
 		uint8_t *, size_t *, size_t)
 		lzma_nothrow;
 
 
-extern lzma_ret lzma_easy_buffer_encode(
-		uint32_t, lzma_check,
-		const lzma_allocator *,
+extern SERIVCE_lzma_ret SERIVCE_lzma_easy_buffer_encode(
+		uint32_t, SERIVCE_lzma_check,
+		const SERIVCE_lzma_allocator *,
 		const uint8_t *, size_t,
 		uint8_t *, size_t *, size_t) lzma_nothrow;
 
